f2b192d260677ee05315da345742930b2337b21b
Mohammad Rayyaan Jamil <mdrayyanjamil355@gmail.com>
2026-06-24 21:18:19 +0530

    security: remove temp files with exposed MongoDB credentials and update .gitignore

--- server/temp_query.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-mongoose.connect('mongodb+srv://admin:Password%40123@ksridealab.fpfklnn.mongodb.net/idealab?retryWrites=true&w=majority&appName=KSRidealab')
-  .then(async () => {
-    const db = mongoose.connection.db;
-    const bookings = await db.collection('bookingrequests').find({'status': 'approved'}).toArray();
-    console.log(JSON.stringify(bookings, null, 2));
-    process.exit(0);
-  });
